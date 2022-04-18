abed1ca5022f503fedaae67ce4ea3e8b689e4237
Abdul Moiz Lakhani <lakhaniabdulmoiz@gmail.com>
2022-04-18 17:58:50 +0500

    created ui and logic to add member

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -3,8 +3,25 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
   title = 'teams-generator';
+  memberName = '';
+  members: string[] = [];
+  errorMessage = '';
+
+  addMember() {
+    if (!this.memberName) {
+      this.errorMessage = 'Please enter a name';
+      return;
+    }
+    this.members.push(this.memberName);
+    this.memberName = '';
+  }
+
+  onInput(member: string) {
+    this.memberName = member;
+    this.errorMessage = '';
+  }
 }
